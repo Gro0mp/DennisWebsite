2c4e06a216002b71c5032f103b8af26a0155ce01
dennisw002 <dennis.wong002@umb.edu>
2025-12-31 15:19:23 -0500

    update footer

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -5,12 +5,14 @@ export const Footer = () => {
     return (
         <section id="footer" className="w-full bg-black bg-opacity-70 text-gray-300 text-center py-2">
             <RevealOnScroll>
-                <div className="max-w-screen-xl mx-auto px-4">
+                <div className={`max-w-3xl mx-auto px-4`}>
                     <h2>
                         © {new Date().getFullYear()} Dennis Wong. All rights reserved.
                     </h2>
                     <h2>
                         Model from: https://codesandbox.io/p/sandbox/ykfpwf
+                    </h2>
+                    <h2 className={`text-3xl font-bold mb-8 bg-clip-text text-transparent text-center`}>
                     </h2>
                 </div>
             </RevealOnScroll>
